6b7d334d81c3619debd7076c757a7f3b0c67cb64
hangtingchen <465244243@qq.com>
2017-09-24 00:09:57 +0800

    finish VAD Detection and audio saving

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<math.h>
+#include<string.h>
 #include"fft.h"
 #include"hmath.h"
 #include"sigProcess.h"
@@ -12,21 +13,31 @@ typedef struct {
 	struct activeSeg* link;
 }activeSeg;
 
+double frameSizeInTime = 0.025; 
+double frameShiftSizeInTime = 0.010;
+double MH = (double)1e5, ML = (double)1e4, Zs = 0.06;
+double gapLeast = 0.11;
+
+
 int main(int argc, char** argv) {
 	printf("%s\n", argv[0]);
 	printf("This program is for VAD. Input the audio to be detected and the dir to store the transformed audio.\n ");
+	printf("usage: VAD.exe <intput WAVE file> <output dir>\n");
+
 	//open WAVE and read
+	printf("Open WAVE file\n");
 	WAVE_t w = initWAVE_t();
 	FILE* fWAVE = NULL;
-	if (argc >= 2) { fWAVE = fopen(argv[1], "rb"); printf("WAVE information:\nname: %s\n", argv[1]);}
-	else { fWAVE = fopen("PHONE_001.wav", "rb"); printf("WAVE information:\nname: PHONE_001.wav\n");}
+	if (argc >= 2) { fWAVE = fopen(argv[1], "rb"); printf("\nWAVE information:\nname: %s\n", argv[1]);}
+	else { fWAVE = fopen("PHONE_001.wav", "rb"); printf("\nWAVE information:\nname: PHONE_001.wav\n");}
 	loadWAVEFile(&w, fWAVE);
 	print_WAVE(w);
 
+	printf("\nConfiguration\n");
 	//Frame Signal
-	double frameSizeInTime = 0.025; double frameShiftSizeInTime = 0.010;
 	int frameSize = (int)(frameSizeInTime*(double)w.WAVEParams.sampleRate); int frameShiftSize = (int)(frameShiftSizeInTime*(double)w.WAVEParams.sampleRate);
 	if (w.WAVEParams.numChannels > 1) { printf("Only use one channel\n"); }
+	printf("Frame length : %f s\nFrame shift length : %f s \n", frameSizeInTime, frameShiftSizeInTime);
 	Matrix frameStackOfSignal = frameRawSignal(w.DATA.data[1], frameSize, frameShiftSize);
 	int numFrames = NumRows(frameStackOfSignal);
 
@@ -49,9 +60,9 @@ int main(int argc, char** argv) {
 	*/
 
 	//set MH,ML and Zs
-	double MH =(double)1e5, ML = (double)1e4, Zs = 0.06;
 	IntVec VMH = CreateIntVec(numFrames); IntVec VML = CreateIntVec(numFrames);
 	IntVec VZs = CreateIntVec(numFrames); 
+	printf("MH = %e \nML = %e \nZs = %e \n", MH, ML, Zs);
 	IntVec speechInd = CreateIntVec(numFrames);//Vector to store wheather the frame contains speech
 	for (int i = 1; i <= numFrames; i++) {
 		VMH[i] = energyForFrames[i] > MH ? 1 : 0;
@@ -86,7 +97,8 @@ int main(int argc, char** argv) {
 	}
 
 	//if the gap between two speech segments is less than gapLeast,combine the segments into one
-	pos = 1; double gapLeast = 1.0;
+	pos = 1;
+	printf("Time gap between segment : %f\n", gapLeast);
 	for (int i = 2; i <= numFrames - 1; i++) {
 		if (speechInd[i - 1] == 1 && speechInd[i] == 0) pos = i;
 		if (speechInd[i] == 0 && speechInd[i + 1] == 1&&pos!=0) {
@@ -96,19 +108,19 @@ int main(int argc, char** argv) {
 	}
 
 	//print the silent segments
-	pos = 0;
-	printf("the silence segments\n");
-	for (int i = 2; i <= numFrames; i++) {
-		if (speechInd[i-1]&&!speechInd[i])pos = i;//10
-		if (speechInd[i]&&pos) {//01
-			printf("%f --- %f \n", frameShiftSizeInTime*(double)(pos-1), frameShiftSizeInTime*(double)i);
-			pos = 0;
-		}
-	}
+	//pos = 0;
+	//printf("the silence segments\n");
+	//for (int i = 2; i <= numFrames; i++) {
+	//	if (speechInd[i-1]&&!speechInd[i])pos = i;//10
+	//	if (speechInd[i]&&pos) {//01
+	//		printf("%f --- %f \n", frameShiftSizeInTime*(double)(pos-1), frameShiftSizeInTime*(double)i);
+	//		pos = 0;
+	//	}
+	//}
 
 	//print the speech segments and store these information into a linkList
 	pos = 1; activeSeg* head = NULL; activeSeg* tail = NULL; activeSeg* tempP = NULL;
-	printf("the voice active segments\n");
+	printf("\nthe voice active segments : \n");
 	for (int i = 2; i <= numFrames-1; i++) {
 		if (!speechInd[i - 1] && speechInd[i])pos = i;//01
 		if (speechInd[i] && (!speechInd[i+1] || i== numFrames - 1) &&pos) {	//10
@@ -121,18 +133,48 @@ int main(int argc, char** argv) {
 				tail->link = tempP; tail = tempP;
 			}
 			
-			printf("%f --- %f \n", frameShiftSizeInTime*(double)(pos), frameShiftSizeInTime*(double)(i));
+			printf("\t%f --- %f \n", frameShiftSizeInTime*(double)(pos), frameShiftSizeInTime*(double)(i));
 			pos = 0;
 			
 		}
 	}
 
-	
+
+	//save audio files
+	char saveFileDes1[300] = { '\0' }; char saveFileDes2[] = { '\\','\0' }; char saveFileDes3[5] = { '\0' }; char saveFileDes4[6] = { '.','w','a','v','\0' };
+	FILE* waveForWrite = NULL;
+	int saveFileCounter = 0;
+	WAVEParams_t paramsForWrite = w.WAVEParams;
+	IntMat samplesForWrite = NULL;
 	tempP = head;
-	do {
-		//pass
+	printf("\nBegin writing audio\n");
+	while (tempP) {
+		saveFileCounter++;
+		if (argc >= 3) { 
+			strcpy(saveFileDes1, argv[2]);
+			strcat(saveFileDes1, saveFileDes2);
+		}
+		else saveFileDes1[0] = '\0';
+
+		_itoa(saveFileCounter, saveFileDes3,10);
+		strcat(saveFileDes1, saveFileDes3);
+		strcat(saveFileDes1, saveFileDes4);
+
+		waveForWrite = fopen(saveFileDes1, "wb");
+		paramsForWrite.numSamples = tempP->endSampleNum - tempP->startSampleNum + 1;
+		samplesForWrite = CreateIntMat(paramsForWrite.numChannels, paramsForWrite.numSamples);
+		for (int i = tempP->startSampleNum; i <= tempP->endSampleNum; i++)for (int j = 1; j <= paramsForWrite.numChannels; j++)samplesForWrite[j][i - tempP->startSampleNum + 1] = w.DATA.data[j][i];
+		writeWaveFile(waveForWrite, paramsForWrite, samplesForWrite);
+		fclose(waveForWrite);
+		printf("\tWriting file %s\n", saveFileDes1);
+
+		FreeIntMat(samplesForWrite);
 		tempP = tempP->link;
-	} while (tempP);
+
+
+	}
+	
+	
 
 	//free linkList
 	do {
